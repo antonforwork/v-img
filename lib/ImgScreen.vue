b79bd4c3127b946fee0af4e5c97090168e8fa93a
antonforwork <me@anton.in.ua>
2019-06-04 18:48:36 +0300

    css style

--- lib/ImgScreen.vue
+++ lib/ImgScreen.vue
@@ -62,7 +62,7 @@
       </div>
 
       <div class="content-v-img" style="width: 100vw; height: 100vh;">
-        <vue-zoomer style="border: solid 1px silver;height: 100%;">
+        <vue-zoomer style="height: 100%;">
           <img :src="images[currentImageIndex]" @click="next" style="object-fit: contain; width: 100%; height: 100%;">
         </vue-zoomer>
       </div>
